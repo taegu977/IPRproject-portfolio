b5b303bff1ee143aa84601f95ebdd003ac842509
taegu977 <mykim4110@daum.net>
2025-05-23 16:22:15 +0900

    Add files via upload

---
+++ ptapplication/script2.js
@@ -0,0 +1,74 @@
+function toggleHelp() {
+    var helpContent = document.getElementById("ptapplication");
+
+    // helpContent이 보이면 숨기고, 숨겨져 있으면 보이게 함
+    if (helpContent.style.display === "none" || helpContent.style.display === "") {
+        helpContent.style.display = "block";  // 내용 표시
+    } else {
+        helpContent.style.display = "none";   // 내용 숨기기
+    }
+}
+
+function toggleHelp1() {
+    var helpContent1 = document.getElementById("ptapplication1");
+
+    // 보이거나 숨김 상태를 토글
+    if (helpContent1.style.display === "none" || helpContent1.style.display === "") {
+        helpContent1.style.display = "block";
+    } else {
+        helpContent1.style.display = "none";
+    }
+}
+
+function toggleHelp2() {
+    var helpContent2 = document.getElementById("ptapplication2");
+
+    // 보이거나 숨김 상태를 토글
+    if (helpContent2.style.display === "none" || helpContent2.style.display === "") {
+        helpContent2.style.display = "block";
+    } else {
+        helpContent2.style.display = "none";
+    }
+}
+
+function toggleHelp3() {
+    var helpContent3 = document.getElementById("ptapplication3");
+
+    // 보이거나 숨김 상태를 토글
+    if (helpContent3.style.display === "none" || helpContent3.style.display === "") {
+        helpContent3.style.display = "block";
+    } else {
+        helpContent3.style.display = "none";
+    }
+}
+
+function toggleHelp4() {
+    var helpContent4 = document.getElementById("ptapplication4");
+
+    // 보이거나 숨김 상태를 토글
+    if (helpContent4.style.display === "none" || helpContent4.style.display === "") {
+        helpContent4.style.display = "block";
+    } else {
+        helpContent4.style.display = "none";
+    }
+}
+function toggleHelp5() {
+    var helpContent5 = document.getElementById("ptapplication5");
+
+    // 보이거나 숨김 상태를 토글
+    if (helpContent5.style.display === "none" || helpContent5.style.display === "") {
+        helpContent5.style.display = "block";
+    } else {
+        helpContent5.style.display = "none";
+    }
+}
+function toggleHelp6() {
+    var helpContent6 = document.getElementById("ptapplication6");
+
+    // 보이거나 숨김 상태를 토글
+    if (helpContent6.style.display === "none" || helpContent6.style.display === "") {
+        helpContent6.style.display = "block";
+    } else {
+        helpContent6.style.display = "none";
+    }
+}
